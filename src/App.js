c03b93cd2b99516d117813c13ed05b19677d1cb0
Kathy Bradt <kathybradt717@gmail.com>
2021-05-27 10:13:24 -0500

    removed extra code from app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import {Route, Link} from 'react-router-dom';
+import {Route} from 'react-router-dom';
 import AllStates from './components/AllStates/AllStates';
 import LandingPage from './components/LandingPage/LandingPage.js';
 import SingleState from './components/SingleState/SingleState';
@@ -9,16 +9,11 @@ import Header from './components/Header/Header';
 function App() {
   return (
     <main className='App'>
-      <div>
-        <Header/>
-        <div>
-          <Route exact path={"/"} component={LandingPage}/>
-          <Route exact path={"/covid_project"} component={LandingPage}/>
-          <Route path={"/allstates"} component={AllStates} />
-          <Route path={"/singlestate"} component={SingleState}/>
-          <Route path={"/countryvaccinations"} component={CountryVaccPie}/>
-        </div>
-      </div>
+      <Header/>
+      <Route exact path={"/"} component={LandingPage}/>
+      <Route exact path={"/covid_project"} component={LandingPage}/>
+      <Route path={"/allstates"} component={AllStates} />        <Route path={"/singlestate"} component={SingleState}/>
+      <Route path={"/countryvaccinations"} component={CountryVaccPie}/>
     </main>
   );
 }

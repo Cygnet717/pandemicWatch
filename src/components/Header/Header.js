4b8e9530d6383bd7dfc5c274e6b472ae3a058fde
Kathy Bradt <kathybradt717@gmail.com>
2021-06-02 09:52:49 -0500

    change to home link

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ export default class Header extends Component {
     render() {
         return <>
         <nav>
-          <Link className="headerLink" to="/covid_project">Home</Link>
+          <Link className="headerLink" to="/pandemicwatch">Home</Link>
           <Link className="headerLink" to="/allstates">All States</Link>
           <Link className="headerLink" to="/singlestate">Single State</Link>
           <Link className="headerLink" to="/countryvaccinations">USA Vaccinations</Link>
